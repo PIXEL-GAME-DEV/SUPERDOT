41cec2864d858f83183ab4080ac8a5cd866e5a73
Gleb Sidorov <105152340+PIXEL-GAME-DEV@users.noreply.github.com>
2025-04-03 22:38:24 +1100

    Create SUPERDOT project
    Create project, folder structure, player scene and script.

---
+++ script/object/node/node_3d/collision_object_3d/physics_body_3d/rigid_body_3d/player.gd
@@ -0,0 +1,17 @@
+extends RigidBody3D
+
+
+func _enter_tree() -> void:
+	pass
+
+
+func _exit_tree() -> void:
+	pass
+
+
+func _ready() -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
