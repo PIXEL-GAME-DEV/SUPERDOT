b341d97dede5d207e408d0407638e0aa0ef6c4eb
Gleb Sidorov <105152340+PIXEL-GAME-DEV@users.noreply.github.com>
2025-04-08 22:22:18 +1000

    More code!
    Movement code changed, pixelated rendering and outline shader.

--- script/object/node/node_3d/collision_object_3d/physics_body_3d/rigid_body_3d/player.gd
+++ script/object/node/node_3d/collision_object_3d/physics_body_3d/rigid_body_3d/player.gd
@@ -4,11 +4,14 @@ extends RigidBody3D
 @export var _mouse_dpi: int = 1000
 
 
+var gravity := Vector3.ZERO 
+
 var _look := Vector3.ZERO
 
 var _move := Vector3.ZERO
 
 var _time_speed := 0.0
+
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -16,6 +19,12 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
+	gravity = get_gravity()
+	
+	var _basis_x = %Orientation.global_basis.x
+	var _basis_y = %Orientation.global_basis.y
+	var _basis_z = %Orientation.global_basis.z
+	
 	var _roll := Input.get_axis("-roll", "roll")
 	_look.z = _roll / 60
 	
@@ -25,16 +34,23 @@ func _process(_delta: float) -> void:
 	
 	%Orientation.rotate_object_local(Vector3.UP, _look.x)
 	%Orientation.rotate_object_local(Vector3.RIGHT, _look.y)
-	%Orientation.rotate_object_local(Vector3.FORWARD, _look.z)
+	#%Orientation.rotate_object_local(Vector3.FORWARD, _look.z)
 	
+	var _plane := Plane(%Orientation.global_basis.z)
+	
+	var _angle = _basis_y.signed_angle_to(_plane.project(-gravity), -_basis_z)
+	
+	%Orientation.rotate_object_local(Vector3.FORWARD, _angle)
+	
+	%Camera3D.global_position = global_position + global_basis.y * 0.6
 	%Camera3D.global_basis = %Orientation.global_basis
 
 
 func _physics_process(_delta: float) -> void:
-	var _gravity = get_gravity()
+	gravity = get_gravity()
 	apply_central_force(%Orientation.global_basis * _move * mass)
-	apply_force(-_gravity * mass, global_basis.y)
-	apply_force(_gravity * mass, -global_basis.y)
+	apply_force(-gravity * mass, global_basis.y)
+	apply_force(gravity * mass, -global_basis.y)
 	
 	_time_speed = remap(clampf(_look.length() + _move.length(), 0, 1), 0, 1, 0.1, 1)
 	Engine.time_scale = _time_speed
